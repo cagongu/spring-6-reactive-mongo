eb1c2071130888c1e7dc8fb699c34ce725b098da
cagongu <hophucthai9@gmail.com>
2024-03-05 14:52:44 +0700

    adding save method

--- src/test/java/guru/springframework/spring6reactivemongo/services/BeerServiceImplTest.java
+++ src/test/java/guru/springframework/spring6reactivemongo/services/BeerServiceImplTest.java
@@ -3,8 +3,11 @@ package guru.springframework.spring6reactivemongo.services;
 
 import guru.springframework.spring6reactivemongo.domain.Beer;
 import guru.springframework.spring6reactivemongo.mappers.BeerMapper;
+import guru.springframework.spring6reactivemongo.mappers.BeerMapperImpl;
 import guru.springframework.spring6reactivemongo.model.BeerDTO;
+import guru.springframework.spring6reactivemongo.repositories.BeerRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +16,7 @@ import reactor.core.publisher.Mono;
 import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.awaitility.Awaitility.await;
 
@@ -25,34 +29,45 @@ class BeerServiceImplTest {
     @Autowired
     BeerMapper beerMapper;
 
+    @Autowired
+    BeerRepository beerRepository;
+
     BeerDTO beerDTO;
+
     @BeforeEach
     void setUp() {
         beerDTO = beerMapper.beerToBeerDto(getTestBeer());
     }
 
     @Test
-    void saveBeer() {
-
+    @DisplayName("Test Save Beer Using Subcriber")
+    void saveBeerUseSubcriber() {
         AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<BeerDTO> atomicDto = new AtomicReference<>();
 
         Mono<BeerDTO> savedMono = beerService.saveBeer(Mono.just(beerDTO));
 
-        savedMono.subscribe(savedDto -> {
+        savedMono.subscribe(savedDto ->{
             System.out.println(savedDto.getId());
-            atomicBoolean.set(true);
+
         });
-
-        await().untilTrue(atomicBoolean);
-
     }
-    public static Beer getTestBeer() {
+
+    public BeerDTO getSavedBeerDto(){
+        return beerService.saveBeer(Mono.just(getTestBeerDto())).block();
+    }
+
+    public static BeerDTO getTestBeerDto(){
+        return new BeerMapperImpl().beerToBeerDto(getTestBeer());
+    }
+
+    public static Beer getTestBeer(){
         return Beer.builder()
                 .beerName("Space Dust")
                 .beerStyle("IPA")
                 .price(BigDecimal.TEN)
                 .quantityOnHand(12)
-                .upc("123213")
+                .upc("123423")
                 .build();
     }
 }
